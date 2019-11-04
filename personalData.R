4c89f3be8a985cc2a8e2b99474e7a19fc3087a94
Ramesh Chellaton <rameshchellaton@rameshs-mbp.lan>
2019-11-03 21:33:42 -0500

    commit after time series plot.

--- personalData.R
+++ personalData.R
@@ -14,3 +14,10 @@ PAdata_summary <- summarize(PAdata_bg, totalSteps=sum(steps, na.rm=TRUE))
 p <- ggplot(PAdata_summary, aes(x=date, y=totalSteps))+
     geom_bar(stat="identity", color="dark blue", fill="light blue")
 print(p)
+
+# get means and medians
+s1 <- summarize(PAdata_bg, mean(steps, na.rm=TRUE), median(steps, na.rm=TRUE))
+names(s1) <- c("date", "means", "medians")
+# time series plot of averages
+p2 <- ggplot(s1, aes(x=date, y=means, na.rm=TRUE )) + geom_bar(stat="identity", color="dark blue", fill="steelblue4")
+print(p2)
