43e7ded67f1395f9c4d5ba3bff8376e6d3034597
Ramesh Chellaton <rameshchellaton@rameshs-mbp.lan>
2019-11-03 22:05:35 -0500

    after average 5-min interval with max steps

--- personalData.R
+++ personalData.R
@@ -19,5 +19,11 @@ print(p)
 s1 <- summarize(PAdata_bg, mean(steps, na.rm=TRUE), median(steps, na.rm=TRUE))
 names(s1) <- c("date", "means", "medians")
 # time series plot of averages
-p2 <- ggplot(s1, aes(x=date, y=means, na.rm=TRUE )) + geom_bar(stat="identity", color="dark blue", fill="steelblue4")
+p2 <- ggplot(s1, aes(x=date, y=means, na.rm=TRUE )) + 
+  geom_bar(stat="identity", color="dark blue", fill="steelblue4")
 print(p2)
+
+# the 5-minute interval, on an average, with the maximum number of steps
+s2 <- aggregate(steps ~ date, PAdata_bg, max)
+s2 <- merge(s2, PAdata_bg)
+print(mean(s2$interval))
