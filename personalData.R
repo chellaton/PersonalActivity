c5ab56c00e2ffc6f54b2d248253d2205cceaebe4
Ramesh Chellaton <chellaton@yahoo.com>
2019-11-07 17:05:00 -0500

    after panel graphing...

--- personalData.R
+++ personalData.R
@@ -43,9 +43,20 @@ p_impute <- ggplot(PAdata_impute_summ, aes(x=date, y=totalSteps)) +
   geom_bar(stat="identity", color="dark green", fill="green3")
 print(p_impute)
 #
-# Panel plot comparing average number of steps over weekdays vs weekends
+# Panel plot comparing average number of steps by interval over weekdays vs weekends
+# x axis: intervals
+# y axis: average number of steps
 #
+PAdata_int <- PAdata %>% mutate(Weekday=ifelse(weekdays(date)=="Saturday" |
+                                                 weekdays(date)=="Sunday",0,1))
+PAdata_int <- PAdata_int %>% group_by(interval, Weekday)
+PAdata_int_summary <- PAdata_int %>% summarize(AvgSteps=mean(steps, na.rm=TRUE))
+wkdayPlot <- ggplot(filter(PAdata_int_summary,Weekday==1),aes(x=interval, y=AvgSteps))+
+  geom_line(col="dark blue") + ylab("Average Steps on Weekdays")+geom_smooth(method="auto")
+wkendPlot <- ggplot(filter(PAdata_int_summary,Weekday==0),aes(x=interval, y=AvgSteps))+
+  geom_line(col="dark blue") + ylab("Average Steps on Weekends")+geom_smooth(method="auto")
+
 # load library gridExtra
 library(gridExtra)
-grid.arrange(p, p_impute, ncol=2)
+grid.arrange(wkdayPlot, wkendPlot, ncol=2)
 # All of R code in the report
